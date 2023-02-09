aa85b04b16ac1c5cbf3402cb83caf3b55e157707
Levik Khachadoorian <Levik.khachadoorian.870@my.csun.edu>
2023-02-08 23:38:01 -0800

    newVersion

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { useEffect, useState } from "react";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import "typeface-roboto";
@@ -20,81 +20,69 @@ const styles = {
   },
 };
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      quotes: [],
-      selectQuoteIndex: null,
-    };
-    this.assingNewQuoteIndex = this.assingNewQuoteIndex.bind(this);
-  }
+function App({ classes }) {
+  const [quotes, setQuotes] = useState([]);
+  const [selectQuoteIndex, setSelectedQuoteIndex] = useState(null);
 
-  componentDidMount() {
-    fetch(
+  useEffect(async () => {
+    const data = await fetch(
       "https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json"
-    )
-      .then((data) => data.json())
-      .then((quotes) =>
-        this.setState({ quotes: quotes }, this.assingNewQuoteIndex)
-      );
-  }
-
-  selectQuoteIndex() {
-    return Math.floor(Math.random() * this.state.quotes.length);
-  }
-
-  assingNewQuoteIndex() {
-    this.setState({ selectQuoteIndex: this.selectQuoteIndex() });
-  }
-
-  render() {
-    return (
-      <Grid className={this.props.classes.containera} id="quote-box" container>
-        <Grid xs={11} lg={8} item>
-          {this.state.quotes[this.state.selectQuoteIndex] ? (
-            <Card style={{ backgroundColor: "#FAFGET" }}>
-              <CardContent>
-                <Typography id="text">
-                  {this.state.quotes[this.state.selectQuoteIndex].quote}-
-                  <span id="author">
-                    {this.state.quotes[this.state.selectQuoteIndex].author}
-                  </span>
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button
-                  id="new-quote"
-                  style={{
-                    borderRadius: 10,
-                    backgroundColor: " #FFFFF",
-                    padding: "5px 3px",
-                    fontSize: "13px",
-                  }}
-                  size="small"
-                  onClick={this.assingNewQuoteIndex}
-                >
-                  Next Quote
-                </Button>
-                <IconButton
-                  id="tweet-quote"
-                  target="_blank"
-                  href={`https://twitter.com/intent/tweet?text=${
-                    this.state.quotes[this.state.selectQuoteIndex].quote
-                  }`}
-                >
-                  <FontAwesomeIcon
-                    icon={faTwitter}
-                    size="lg"
-                    color="#26a7de"
-                  ></FontAwesomeIcon>
-                </IconButton>
-              </CardActions>
-            </Card>
-          ) : null}
-        </Grid>
-      </Grid>
     );
+    const quotes = await data.json();
+    setQuotes(quotes);
+    assingNewQuoteIndex(Math.random() * quotes.length);
+  }, []);
+
+  function generateQuoteIndex() {
+    return Math.floor(Math.random() * quotes.length);
   }
+
+  function assingNewQuoteIndex() {
+    setSelectedQuoteIndex(generateQuoteIndex());
+  }
+
+  return (
+    <Grid className={classes.containera} id="quote-box" container>
+      <Grid xs={11} lg={8} item>
+        {quotes[selectQuoteIndex] ? (
+          <Card style={{ backgroundColor: "#FAFGET" }}>
+            <CardContent>
+              <Typography id="text">
+                {quotes[selectQuoteIndex].quote}-
+                <span id="author">{quotes[selectQuoteIndex].author}</span>
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button
+                id="new-quote"
+                style={{
+                  borderRadius: 10,
+                  backgroundColor: " #FFFFF",
+                  padding: "5px 3px",
+                  fontSize: "13px",
+                }}
+                size="small"
+                onClick={assingNewQuoteIndex}
+              >
+                Next Quote
+              </Button>
+              <IconButton
+                id="tweet-quote"
+                target="_blank"
+                href={`https://twitter.com/intent/tweet?text=${quotes[selectQuoteIndex].quote}`}
+              >
+                <FontAwesomeIcon
+                  icon={faTwitter}
+                  size="lg"
+                  color="#26a7de"
+                ></FontAwesomeIcon>
+              </IconButton>
+            </CardActions>
+          </Card>
+        ) : null}
+      </Grid>
+    </Grid>
+  );
 }
+
 export default withStyles(styles)(App);
